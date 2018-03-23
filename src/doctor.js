73329b3ccdac572f22c772ef425a23aab4cfe0c1
David Lewis <davidlewisp@gmail.com>
2018-03-23 15:49:56 -0700

    cannot get website key call to work from api, thinking there might be a bug with this one

--- src/doctor.js
+++ src/doctor.js
@@ -43,6 +43,7 @@ export class Doctor {
     console.log(body.data[0].profile.image_url);
     console.log(body.data[0].practices[0].visit_address.city);
     console.log(body.data[0].practices[0].phones[0].number);
+    console.log(body.data[0].practices[0].website);
     let profiles = []
     body.data.forEach(function(practice) {
       profiles.push(practice);
--- src/ui.js
+++ src/ui.js
@@ -1,22 +1,23 @@
 export class userDisplay{
 
 
-  displayNames(doctors, addressConstructor) {
+  displayNames(doctors) {
     doctors.forEach(function(doctor){
       let firstName = doctor.profile.first_name;
       let lastName = doctor.profile.last_name;
-      let newPatientStatus = "not accepting new patients";
+      let phoneNum = doctor.practices[0].phones[0].number
+      let addressStr = doctor.practices[0].visit_address.street + ", " + doctor.practices[0].visit_address.city + ", " + doctor.practices[0].visit_address.state + ", " + doctor.practices[0].visit_address.zip;
 
+      if (doctor.practices[0].visit_address.street2) {
+        addressStr = doctor.practices[0].visit_address.street + ", " + doctor.practices[0].visit_address.street2 + ", " + doctor.practices[0].visit_address.city + ", " + doctor.practices[0].visit_address.state + ", " + doctor.practices[0].visit_address.zip;
+      }
+
+      let newPatientStatus = "not accepting new patients";
       if (doctor.practices[0].accepts_new_patients) {
         newPatientStatus = "is accepting new patients";
       }
 
-      $(".output").append("<p>" + firstName + " " + lastName + " " + newPatientStatus + " " + addressStr + "</p>");
+      $(".output").append("<p>" + firstName + " " + lastName + " " + newPatientStatus + " at " + addressStr + ", Phone number: " + phoneNum + "</p>");
     })
-  }
-
-  addressConstructor(doctor) {
-    let addressStr = doctor.practices[0].visit_address.street + ", " + doctor.practices[0].visit_address.street2 + ", " + doctor.practices[0].visit_address.city + ", " + doctor.practices[0].visit_address.state + ", " + doctor.practices[0].visit_address.zip;
-    return addressStr;
   }
 }
