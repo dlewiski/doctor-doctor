47885ed40b42dbf49af8e39d7e26ac9ec95ad1fc
David Lewis <davidlewisp@gmail.com>
2018-03-23 13:46:00 -0700

    test out how extract the doctor name from the api response

--- src/doctor.js
+++ src/doctor.js
@@ -6,7 +6,7 @@ export class Doctor {
   doctorCall(symptom){
     return new Promise(function(resolve, reject){
       let request = new XMLHttpRequest();
-      let url =`https://api.betterdoctor.com/2016-03-01/doctors?query=${symptom}&location=${this.location}&user_location=45.523%2C-122.6765&skip=0&limit=10&user_key=${process.env.exports.apiKey}`;
+      let url =`https://api.betterdoctor.com/2016-03-01/doctors?query=${symptom}&location=or-portland&user_location=45.523%2C-122.6765&skip=0&limit=10&user_key=${process.env.exports.apiKey}`;
 
       request.onload = function() {
         if (this.status === 200) {
@@ -22,10 +22,11 @@ export class Doctor {
 
   doctorInfo(response){
     let body = JSON.parse(response);
-    let colorArr = [];
-    body.colors.forEach(function(element) {
-        colorArr[element.name] = 0;
-      });
-      return colorArr;
+    console.log(body.data[0].practices[0].name);
+    let doctors = body.data.practices;
+    // body.colors.forEach(function(element) {
+    //     colorArr[element.name] = 0;
+    //   });
+      return doctors;
   }
 }
--- src/main.js
+++ src/main.js
@@ -1,1 +1,19 @@
 import { Doctor } from './doctor.js';
+
+$(document).ready(function(){
+  $('#submit-symptom').submit(function(event){
+    event.preventDefault();
+    let newDoctors = new Doctor();
+    //console.log(newSearch.place);
+    let doctorPromise = newDoctors.doctorCall($('#symptom').val())
+    doctorPromise.then(function(response){
+      let doctors = newDoctors.doctorInfo(response)
+      $(".output").append("<p>" + doctors + "</p>");
+      // promise.then(function(response){
+      //   let stolenColors = newSearch.displayEachBikeColor(response, colorArr);
+      //   display.displayColors(stolenColors);
+      });
+      //promise.error();
+    });
+  //  colorPromise.error();
+  });
